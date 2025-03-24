adbf839867ccdaa8d9a8e68064d2dcdd15e1f498
Yams9 <sambababaly11@gmail.com>
2025-03-24 17:39:22 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_j7sqy8w";
+    const templateID = "template_7dzi4g9";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+      document.getElementById('confirmation-message').style.display = 'block';
+  
+  }
